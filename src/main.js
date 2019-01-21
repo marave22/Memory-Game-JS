ef131f4cbc39bb6991804bad5ede66d45faf5712
Mariam <36631465+marave22@users.noreply.github.com>
2019-01-22 01:52:12 +0400

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -14,16 +14,16 @@ function shuffle(a) {
 function restartGame() {
     shuffle(picturesArray);
     let path = "../images/";
-    for (let i = 0; i< picturesArray.length; i++){
+    for (let i = 0; i < picturesArray.length; i++){
         let inner = `<div class="flip-card">
-            <div class="flip-card-inner">
-                <div class="containPic front">
-                    <img src="${path+picturesArray[i]}" alt="Second image">
-                </div>
-                <div class="containBack back"></div>
-                </div>
-            </div>
-        `;
+                        <div class="flip-card-inner">
+                            <div class="containPic front">
+                                <img src="${path+picturesArray[i]}" alt=Image" class="image">
+                             </div>
+                             <div class="containBack back"></div>
+                         </div>
+                     </div>
+                    `;
 
         document.getElementById('main').innerHTML = document.getElementById('main').innerHTML + inner;
     }
@@ -33,11 +33,10 @@ restartGame();
 
 function run() {
     let visibleInner = document.getElementsByClassName('flip-card-inner');
-
     for (let i = 0; i < visibleInner.length; i++) {
         if (visibleInner[i].classList.toggle("flipped")) {
             visibleInner[i].addEventListener('click', () => {visibleInner[i].classList.toggle("flipped")});
-
+            //console.log(visibleInner[i].classList.toggle("flipped"));
         } else {
             return false;
         }
@@ -46,14 +45,29 @@ function run() {
 
 setTimeout(run, 3000);
 
-// function checkGame() {
-//     let visibleFront = document.getElementsByClassName('front');
-//     for (let i = 0; i < visibleFront.length; i++) {
-//         visibleFront[i].addEventListener('click', () => {visibleFront[i].classList.toggle("flippeda")});
-//         if (visibleFront[i].classList.toggle("flipped")) {
-//             console.log(visibleFront[i].classList.toggle('flipped'));
-//         }
-//     }
-//     console.log(visibleFront);
-// }
-// checkGame();
+function openCard() {
+    let openedCards = [];
+    let len = 2;
+    openedCards.length = len;
+    let visibleCard = document.getElementsByClassName('flip-card-inner');
+    let visibleImage = document.getElementsByClassName('image');
+    for (let i = 0; i < visibleCard.length; i++) {
+        if (visibleCard[i].classList.toggle("flip")) {
+            visibleCard[i].addEventListener('click', () => {visibleCard.classList.toggle("flip")});
+            openedCards =
+            for (let j = 0; j < openedCards.length; j++) {
+                if (openedCards[j].src === openedCards[j + 1].src) {
+                    visibleCard[j].classList.toggle("flip", true);
+                    visibleCard[j + 1].classList.toggle("flip", true);
+                } else {
+                    setInterval(function () {
+                        visibleCard[j].classList.toggle("flip");
+                        visibleCard[j + 1].classList.toggle("flip");
+                    }, 1000)
+                }
+            }
+        }
+    }
+    console.log(opendedCards);
+}
+openCard();
